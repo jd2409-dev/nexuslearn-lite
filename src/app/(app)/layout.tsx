779a89e02aa02fabdd8d2a533413097a06c83dd0
Jaydan <j240924d@gmail.com>
2025-10-26 08:07:38 +0000

    Make everything centered in the application

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -11,7 +11,7 @@ export default function AppLayout({
       <AppSidebar />
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <Header />
-        <main className="flex-1 gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+        <main className="flex flex-1 justify-center items-center gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
           {children}
         </main>
       </div>
