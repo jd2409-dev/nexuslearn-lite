836588b732c09fd467e70c5ffbff936feed2cf91
Jaydan <j240924d@gmail.com>
2025-10-28 12:05:31 +0000

    69806262-6315b7dbc06fb027.js:1 Uncaught FirebaseError: Firebase: Error (

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,5 @@
 'use client';
 
-import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
@@ -11,9 +10,7 @@ interface FirebaseServices {
   firestore: Firestore | null;
 }
 
-// IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase(): FirebaseServices {
-  // Ensure this only runs on the client
   if (typeof window === 'undefined') {
     return {
       firebaseApp: null,
@@ -21,6 +18,15 @@ export function initializeFirebase(): FirebaseServices {
       firestore: null,
     };
   }
+  
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  };
 
   const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
   const auth = getAuth(app);
